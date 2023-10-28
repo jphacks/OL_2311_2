754422c12d97c1f9c2a76d1f6c57910e784ce465
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-10-29 04:58:55 +0900

    fix: count logic and font size

--- src/App.tsx
+++ src/App.tsx
@@ -61,9 +61,9 @@ function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[url('./assets/confetti.png')] bg-[#1637FC] bg-contain bg-no-repeat bg-center relative">
       <div className="flex text-white justify-center items-center flex-col">
-        <h1 className="text-[54px] font-semibold">会場の総乾杯数</h1>
+        <h1 className="text-[40px] font-semibold">会場の総乾杯数</h1>
         <p className="font-[Chillax] text-[250px] leading-none">
-          {totalCheersCount}
+          {totalCheersCount / 2}
         </p>
         <img src={LogoImage} alt="Kanpai!" />
       </div>
