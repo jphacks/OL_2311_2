2f8c4f3454e214eb150cda0e152d06c4b42c19a1
yuuumiravy <misharv.y@gmail.com>
2023-10-28 23:04:01 +0900

    feat: 画面のUIを実装（カンパイ以外）

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import LogoImage from "./assets/logo.png";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-[url('./assets/confetti.png')] bg-[#1637FC] bg-contain bg-no-repeat bg-center">
+      <div className="flex text-white justify-center items-center flex-col">
+        <h1 className="text-[54px] font-semibold">会場の総乾杯数</h1>
+        <p className="font-[Chillax] text-[250px] leading-none">555</p>
+        <img src={LogoImage} alt="Kanpai!" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
